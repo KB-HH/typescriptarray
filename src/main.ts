636c9568d7e1e4d7ba996d6c085d4c0df30672ca
kristinabusch <Kristina.Busch@sales-forum.de>
2023-09-26 12:04:06 +0200

    which number console.log added

--- src/main.ts
+++ src/main.ts
@@ -12,14 +12,18 @@ let arrayReduce: number = [10, 20, 30, 40].reduce((a,b) => a+b);
 console.log("total is " + arrayReduce)
 //------------------------------------------------------------------------------------------
 const arraySome : number [] = [1,20,3,4,50,6,7,18];
+const text = ['aufwärts','abwärts','gleich']
 let welcheZahl : number[] = arraySome.filter((zahl) => zahl > 10);
 let resultSome : boolean = arraySome.some((zahl) => zahl > 10);
 const gradeZahl = arraySome.filter(num => num % 2 === 0);
+const isLargeNumber = (zahl) => zahl > 40;
 
 if(resultSome) {
     console.log("Es gibt mindestens eine Zahl die ist größer als 10")
     console.log("und zwar die Zahlen " + welcheZahl + ".")
     console.log("Grade Zahlen lauten wie folgt: " + gradeZahl + ".")
+    console.log("Der Index der größeren Zahl als 40 lautet: " + arraySome.findIndex(isLargeNumber));
+    console.log("hier die verketteten Zahlen " + arraySome.join('-') + " aus dem Array")
 }else{
     console.log("Es gibt keine Zahl die größer als 10 ist")
 }
