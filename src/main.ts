61bfeec52852f91d9587ca6e76ef37a202c1d2d7
kristinabusch <Kristina.Busch@sales-forum.de>
2023-09-26 11:41:41 +0200

    which number console.log added

--- src/main.ts
+++ src/main.ts
@@ -11,13 +11,15 @@ console.log(result)
 let arrayReduce: number = [10, 20, 30, 40].reduce((a,b) => a+b);
 console.log("total is " + arrayReduce)
 //------------------------------------------------------------------------------------------
-const arraySome = [1,20,3,4,50,6,7,18];
-let resultSome : boolean = arraySome.some((zahl) => zahl > 10);
+const arraySome : number [] = [1,20,3,4,50,6,7,18];
 let welcheZahl : number[] = arraySome.filter((zahl) => zahl > 10);
+let resultSome : boolean = arraySome.some((zahl) => zahl > 10);
+const gradeZahl = arraySome.filter(num => num % 2 === 0);
 
 if(resultSome) {
-    console.log("Es gibt mindestens eine Zahl die ist als 10")
-    console.log(" und zwar die Zahlen " + welcheZahl)
+    console.log("Es gibt mindestens eine Zahl die ist größer als 10")
+    console.log("und zwar die Zahlen " + welcheZahl + ".")
+    console.log("Grade Zahlen lauten wie folgt: " + gradeZahl + ".")
 }else{
     console.log("Es gibt keine Zahl die größer als 10 ist")
 }
